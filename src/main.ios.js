21e68dd81e087669f1320c3b352b670598b1cd0b
Igor Tomov <stepennwolf@gmail.com>
2016-05-20 18:27:19 +0300

    Updated ES Lint 'semi' option

--- src/main.ios.js
+++ src/main.ios.js
@@ -2,14 +2,9 @@
  * React Native Webpack Starter Kit
  * https://github.com/jhabdas/react-native-webpack-starter-kit
  */
-import { AppRegistry } from 'react-native'
-import App from './components/App'
+import { AppRegistry } from 'react-native';
+import App from './components/QuizScene';
 
-class Root extends App {
-  static defaultProps = {
-    ...App.defaultProps,
-    instructions: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
-  };
-}
 
-AppRegistry.registerComponent('App', () => Root)
+
+AppRegistry.registerComponent('App', () => App );
